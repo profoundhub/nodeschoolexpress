ca3c434eb69d0b11c598063737b48024faf9ce0f
Daniel Lim <cloud9@profoundideation.com>
2016-08-23 19:02:14 +0000

    update state of program 4

--- program4.js
+++ program4.js
@@ -1,6 +1,8 @@
 /*global res*/ 
     var express = require('express');
     var app = express();
+    var bodyparser = require('body-parser');
     
-    
+    app.use(bodyparser.urlencoded({extended: false}))
+
     app.listen(process.argv[2]);

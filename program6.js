59913a26a74c51d7912b1a68fccdce1efd860ea7
Daniel Lim <cloud9@profoundideation.com>
2016-08-23 19:46:36 +0000

    program 6 js ++

--- program6.js
+++ program6.js
@@ -10,11 +10,15 @@ PUT /message/526aa677a8ceb64569c9d4fb
 
 */
 
+    app.put('/message/:id', function(req, res){...});
+    
     require('crypto')
       .createHash('sha1')
       .update(new Date().toDateString() + id)
       .digest('hex')
       
-    app.put('/path/:NAME', function(req, res){...});
+//    app.put('/path/:NAME', function(req, res){...});
 
     req.params.NAME
+
+app.listen(process.argv[2]);

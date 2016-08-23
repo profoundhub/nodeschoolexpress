96ed700953d1f8bfb82d2b5d6d701da2b4180dde
Daniel Lim <cloud9@profoundideation.com>
2016-08-23 21:37:32 +0000

    program 7

--- program7.js
+++ program7.js
@@ -2,7 +2,11 @@
 WHAT'S IN QUERY
 Exercise 7 of 8 
 */
+var express = require('express');
+var app = express();
 
-req.query.NAME
+    req.query.NAME
+    
+    res.send(object)
 
-res.send(object)
+app.listen(process.argv[2]);

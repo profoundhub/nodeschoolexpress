7515892816822e9d6bf4f10779497c2094f2e096
Daniel Lim <cloud9@profoundideation.com>
2016-08-23 18:31:03 +0000

    update state

--- program3.js
+++ program3.js
@@ -2,15 +2,11 @@
     var express = require('express');
     var app = express();
     
-    app.set('views', path.join(__dirname, 'templates'));
+    app.set('views', process.argv[3]);
     app.set('view engine', 'jade')
-    
-    app.use(express.static(process.argv[3]||path.join(__dirname, 'public')));
     
     app.get('/home', function(req, res) {
       res.render('index', {date: new Date().toDateString()});
-    })
-    
-    
+    });
     
     app.listen(process.argv[2]);
---
+++ program4.js
@@ -0,0 +1,6 @@
+/*global res*/ 
+    var express = require('express');
+    var app = express();
+    
+    
+    app.listen(process.argv[2]);

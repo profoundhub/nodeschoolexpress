4d3211d859b134c0c53132987840fc046d2c8af2
Daniel Lim <cloud9@profoundideation.com>
2016-08-24 00:27:35 +0000

    start program 8 - k

--- program8.js
+++ program8.js
@@ -9,7 +9,7 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 
-app.get('/books', function() {
+app.get('/books', function (req, res) {
     
     fs.readFile(process.argv[3], function (err, content) {
         if (err) {

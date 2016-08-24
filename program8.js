9a785d1e9da554598aeb8452e171f9a48c3ed2ce
Daniel Lim <cloud9@profoundideation.com>
2016-08-24 00:22:22 +0000

    start program 8 -g

--- program8.js
+++ program8.js
@@ -13,20 +13,22 @@ app.get('/books', function() {
     
     fs.readFile(process.argv[3], function (err, content) {
         if (err) {
+            
             res.send(500);
         
         } try {
-        
+            
+            var books = JSON.parse(content.toString());
+            res.json(books);
          
         } catch(error) {
-         
-         
+            
+            res.send(500);
+            
     });
     
     
 });
-
-
 
 object = JSON.parse(string)
 
